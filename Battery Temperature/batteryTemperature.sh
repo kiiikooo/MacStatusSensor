fb580fad862e84383bf1e37a36e589db929f4adf
kiiikooo <kiko@MacBook-Pro.local>
2017-04-18 15:32:17 +0700

    Battery Temperature

---
+++ Battery Temperature/batteryTemperature.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# clear previous commands
+clear
+
+# search AppleSmartBattery temperature sensor from I/O Kit
+temperature=$(ioreg -rn AppleSmartBattery | grep Temperature | cut -d '=' -f 2)
+
+# calculation for temperature (Celsius)
+temp_final=$(echo "scale=2; $temperature/100" | bc)
+
+# output
+echo " "
+echo "----- Apple Smart Battery -----"
+echo "Battery Temperature: $temp_final degrees Celsius"
+echo " "
+
+# quit
+read -n 1 -sp "Press any key to exit..."
+killall Terminal
